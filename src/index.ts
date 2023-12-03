4aa101fe7c4e5e9b3c9ed18f4dc45090d9fbc6ff
bryanachmad <bryanachmad08@gmail.com>
2023-12-03 16:53:14 +0700

    setup husky

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 // console.log('hahahaha')
 
-import express, { type Request, type Application, type Response, type NextFunction } from 'express'
+import express, {
+    type Request,
+    type Application,
+    type Response,
+    type NextFunction
+} from 'express'
 import 'dotenv/config'
 
 const app: Application = express()
-const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+const port: number =
+    process.env.PORT != null ? parseInt(process.env.PORT) : 3000
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello World')
