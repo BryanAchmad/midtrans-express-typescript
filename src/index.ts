6ac5ffbf939a2da91a2b10168df3de98f90e42f2
bryanachmad <bryanachmad08@gmail.com>
2023-12-03 16:19:25 +0700

    dunno

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// console.log('hahahaha')
+
+import express, { type Request, type Application, type Response, type NextFunction } from 'express'
+import 'dotenv/config'
+
+const app: Application = express()
+const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running under ${port} port`)
+})
